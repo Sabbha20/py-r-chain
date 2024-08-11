a422cad83f7141eda0d5233bf5897ff5ace31626
Sabbha20 <sabbha.power1@gmail.com>
2024-08-11 21:30:15 +0530

    changes in crypto_hash

--- crypto_hash.py
+++ crypto_hash.py
@@ -5,12 +5,13 @@ def crypto_hash(*data):
     """
     Return a SHA-256 hash of the given arguments.
     Args:
-        *data: Variable length argument list, each argument can be of any type.
+        data: Variable length argument list, each argument can be of any type.
 
     Returns:
         SHA-256 hash as a hex string.
     """
     # return hashlib.sha256(json.dumps(data).encode("utf-8")).hexdigest()
+    # sorting is done, so that same data even if in different sequence, produce same hash
     stringified_args = sorted(map(lambda d: json.dumps(d, sort_keys=True), data))
     joined_data = ''.join(stringified_args)
 
@@ -19,6 +20,8 @@ def crypto_hash(*data):
 def main():
     print(f"crypto_hash('data'):\t{crypto_hash('data')}")
     print(f"crypto_hash(['data']):\t{crypto_hash(['data'])}")
+    print(f"crypto_hash(1, 'two', [3,4]):\t{crypto_hash(1, 'two', [3,4])}")
+    print(f"crypto_hash('two', [3,4], 1):\t{crypto_hash('two', [3,4], 1)}")
 
 if __name__ == "__main__":
     main()
